53d29f4c90a1e8f576531a7ac37d106b88eec3ce
92SKraken <sebastian.p.ives@gmail.com>
2024-12-30 13:55:35 -0500

    Made Adjustable number of Lines
    Could be made better

--- TKAMB_Code/Assets/NPC_Interact.cs
+++ TKAMB_Code/Assets/NPC_Interact.cs
@@ -10,9 +10,17 @@ public class NPC_Interact : MonoBehaviour
     public GameObject canvas;
     bool player_dectection = false;
 
+    public int NumberOfLines;
+
     public string Line1;
     public string Line2;
     public string Line3;
+    public string Line4;
+    public string Line5;
+    public string Line6;
+    public string Line7;
+    public string Line8;
+    public string Line9;
 
     public PlayerMovement PlayerMovement;
 
@@ -35,9 +43,42 @@ public class NPC_Interact : MonoBehaviour
         {
             canvas.SetActive(true);
             PlayerMovement.dialogue = true;
-            NewDialogue(Line1);
-            NewDialogue(Line2);
-            NewDialogue(Line3);
+            if (NumberOfLines >= 1)
+            {
+                NewDialogue(Line1);
+            }
+            if (NumberOfLines >= 2)
+            {
+                NewDialogue(Line2);
+            }
+            if (NumberOfLines >= 3)
+            {
+                NewDialogue(Line3);
+            }
+            if (NumberOfLines >= 4)
+            {
+                NewDialogue(Line4);
+            }
+            if (NumberOfLines >= 5)
+            {
+                NewDialogue(Line5);
+            }
+            if (NumberOfLines >= 6)
+            {
+                NewDialogue(Line6);
+            }
+            if (NumberOfLines >= 7)
+            {
+                NewDialogue(Line7);
+            }
+            if (NumberOfLines >= 8)
+            {
+                NewDialogue(Line8);
+            }
+            if (NumberOfLines >= 9)
+            {
+                NewDialogue(Line9);
+            }
             canvas.transform.GetChild(1).gameObject.SetActive(true);
         }
     }
